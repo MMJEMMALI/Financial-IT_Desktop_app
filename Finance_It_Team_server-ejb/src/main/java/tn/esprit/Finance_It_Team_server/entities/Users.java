459e57484d5c62d46038aa48ff8e66a723fd7738
ITECH <ITECH@ITECH-PC>
2018-02-21 19:44:40 +0100

    second commit

---
+++ Finance_It_Team_server-ejb/src/main/java/tn/esprit/Finance_It_Team_server/entities/Users.java
@@ -0,0 +1,78 @@
+package tn.esprit.Finance_It_Team_server.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Users implements Serializable{
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String first_name;
+	private String last_name;
+	private String email;
+	private String password;
+	private String address;
+	private String gender;
+	private int phone_number;
+	private String role;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getPhone_number() {
+		return phone_number;
+	}
+	public void setPhone_number(int phone_number) {
+		this.phone_number = phone_number;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
